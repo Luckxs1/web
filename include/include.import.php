578e92bfd7f8a4605cb939ee007fd01216d4857d
Luckxs1 <yunquelucky@Gmail.com>
2023-07-19 16:46:59 +0800

    create

--- include/include.import.php
+++ include/include.import.php
@@ -1,4 +1,4 @@
 <?php
 
-$BASE_DIR = 'http://localhost/GITHUB/web/';
+$BASE_DIR = 'https://10.50.103.155/';
 $variable = "Hello I'am a variable defined from <code>include/include.import.php</code>";
